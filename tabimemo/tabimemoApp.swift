5b588fd3ca53e0828313a3f997d376b7fcb78eba
Xi Wang <xwang@noralta.com>
2024-12-22 15:39:39 -0500

    initialize

---
+++ tabimemo/tabimemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabimemoApp.swift
+//  tabimemo
+//
+//  Created by Wang Xi on 2024/12/22.
+//
+
+import SwiftUI
+
+@main
+struct tabimemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tabimemoTests/tabimemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  tabimemoTests.swift
+//  tabimemoTests
+//
+//  Created by Wang Xi on 2024/12/22.
+//
+
+import Testing
+@testable import tabimemo
+
+struct tabimemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
